6f312c1d6c0c6663f6e49f8c9407ab1ae5c9aedc
GNcode <gngalarga027@gmail.com>
2025-07-11 15:46:15 -0500

    Add product images and database for tortas, malteadas, and helados
    - Added image for 'Ritual de cacao estelar' in the tortas category.
    - Created a new products database file with entries for tortas, malteadas, and helados, including names, prices, and image URLs.

---
+++ src/assets/productsDatabase.ts
@@ -0,0 +1,62 @@
+export const productsDatabase = {
+  tortas: [
+    {
+      id: 1,
+      name: 'Cítrico cósmico',
+      prize: '10.000',
+      imgUrl: `${import.meta.env.BASE_URL}images/productos/tortas/citrico_cosmico.webp`
+    },
+    {
+      id: 2,
+      name: 'Nebulosa de menta',
+      prize: '12.000',
+      imgUrl: `${import.meta.env.BASE_URL}images/productos/tortas/nebulosa_de_menta.webp`
+    },
+    {
+      id: 3,
+      name: 'Ritual de cacao estelar',
+      prize: '14.000',
+      imgUrl: `${import.meta.env.BASE_URL}images/productos/tortas/ritual_de_cacao_estelar.webp`
+    },
+  ],
+  malteadas: [
+    {
+      id: 1,
+      name: 'Órbita cafeinada',
+      prize: '8.000',
+      imgUrl: `${import.meta.env.BASE_URL}images/productos/malteadas/orbita_cafeinada.webp`
+    },
+    {
+      id: 2,
+      name: 'Besos de asteroide',
+      prize: '12.000',
+      imgUrl: `${import.meta.env.BASE_URL}images/productos/malteadas/besos_de_asteroide.webp`
+    },
+    {
+      id: 3,
+      name: 'Mango meteoro',
+      prize: '15.000',
+      imgUrl: `${import.meta.env.BASE_URL}images/productos/malteadas/mango_meteoro.webp`
+    },
+  ],
+  helados: [
+    {
+      id: 1,
+      name: 'Supernova tropical',
+      prize: '10.000',
+      imgUrl: `${import.meta.env.BASE_URL}images/productos/helados/supernova_tropical.webp`
+    },
+    {
+      id: 2,
+      name: 'Trilogía de lunas dulces',
+      prize: '10.000',
+      imgUrl: `${import.meta.env.BASE_URL}images/productos/helados/trilogia_de_lunas_dulces.webp`
+    },    
+    {
+      id: 3,
+      name: 'Sistemático & delicioso',
+      prize: '18.000',
+      imgUrl: `${import.meta.env.BASE_URL}images/productos/helados/sistematico_delicioso.webp`
+    },
+  ]
+}
--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,88 @@
 import { gsap } from "gsap";
+import { productsDatabase } from "./assets/productsDatabase";
 
 const btnTortas = document.getElementById('button_tortas');
 const btnMalteadas = document.getElementById('button_malteadas');
 const btnHelados = document.getElementById('button_helados');
 const mainHomeBtn = document.getElementById('main_home-btn');
+const mainProductsItems = document.getElementById('main_products-items');
+const avatarLeft = document.getElementById('avatar-left');
+const avatarTop = document.getElementById('avatar-top');
+const avatarRight = document.getElementById('avatar-right');
+
+const currentHover = 0;
+
+const tlHover = gsap.timeline({
+  paused:true,
+  defaults: {
+  duration:0.2,
+  ease: 'sine.out',
+  }
+})
+
+const animationHover = (newHover:number)=>{
+  if (currentState != 0) return; //fix this
+  if (newHover == 1) {
+    tlHover.to('#avatar-right, #avatar-top, #button_helados, #button_malteadas, #main_wrapper', {filter: 'grayScale(1)'})
+    .to('#avatar-left', {scale: 1.05}, 0)
+  } else if (newHover == 2) {
+    tlHover.to('#avatar-left, #avatar-right, #button_tortas, #button_helados, #main_wrapper', {filter: 'grayScale(1)'})
+    .to('#avatar-top', {scale: 1.05}, 0)
+  } else {
+    tlHover.to('#avatar-left, #avatar-top, #button_tortas, #button_malteadas, #main_wrapper', {filter: 'grayScale(1)'})
+    .to('#avatar-right', {scale: 1.05}, 0)
+  }
+  tlHover.play();
+}
+const clearAnimationHover = ()=>{
+  if (currentState != 0) return; //fix this
+  tlHover.progress(0);
+  tlHover.clear();
+}
+
+avatarLeft?.addEventListener('mouseenter', ()=>{
+  animationHover(1);
+})
+btnTortas?.addEventListener('mouseenter', ()=>{
+  animationHover(1);
+})
+avatarTop?.addEventListener('mouseenter', ()=>{
+  animationHover(2);
+})
+btnMalteadas?.addEventListener('mouseenter', ()=>{
+  animationHover(2);
+})
+avatarRight?.addEventListener('mouseenter', ()=>{
+  animationHover(3);
+})
+btnHelados?.addEventListener('mouseenter', ()=>{
+  animationHover(3);
+})
+
+avatarLeft?.addEventListener('mouseleave', ()=> {
+  clearAnimationHover()
+})
+btnTortas?.addEventListener('mouseleave', ()=>{
+  clearAnimationHover()
+})
+avatarTop?.addEventListener('mouseleave', ()=> {
+  clearAnimationHover()
+})
+btnMalteadas?.addEventListener('mouseleave', ()=>{
+  clearAnimationHover()
+})
+avatarRight?.addEventListener('mouseleave', ()=> {
+  clearAnimationHover()
+})
+btnHelados?.addEventListener('mouseleave', ()=>{
+  clearAnimationHover()
+})
 
 // control del viewport
-let previousWidth = window.innerWidth; //1024px breakpoint
+let isDesktop = true;
+document.addEventListener('DOMContentLoaded', ()=>{
+  if (window.innerWidth < 1024) isDesktop = false;
+})
 
 let currentState = 0; //Estado base
 // 1 tortas, 2 malteadas, 3 helados
@@ -15,7 +91,7 @@ let currentState = 0; //Estado base
 const tlExitAvatars = gsap.timeline({
   paused: true,
   defaults: {
-    duration: 0.6,
+    duration: 0.4,
     ease: 'back.in',
     autoAlpha: 0,
     top: '-=5%',
@@ -39,7 +115,7 @@ const animationsExitAvatars = ()=>{
 const tlRotate = gsap.timeline({
   paused: true,
   defaults: {
-    duration: 0.6,
+    duration: 0.5,
     ease: 'sine.out'
   }
 })
@@ -100,8 +176,8 @@ const animationsRotate = (avatar:number)=>{
 const tlProducts = gsap.timeline({
   paused: true,
   defaults: {
-    duration: 0.6,
-    ease: 'since.out'
+    duration: 0.4,
+    ease: 'sine.out'
   }
 })
 const animationsProducts = ()=>{
@@ -127,6 +203,8 @@ const animationsProducts = ()=>{
       rotate: '-=90'
     }, '<').set('#main_home-btn', {
       display: 'block'
+    }).set('#main_products h2', {
+      display: 'block'
     })
 }
 
@@ -140,6 +218,7 @@ const playTimeline = (timeline:gsap.core.Timeline)=>{
 
 // Función para revertir todas las animaciones
 const reverseAnimations = async ()=>{
+  if (mainProductsItems) mainProductsItems.innerHTML = '';
   await tlProducts.reverse();
   tlProducts.clear();
   await tlRotate.reverse();
@@ -148,6 +227,41 @@ const reverseAnimations = async ()=>{
   tlExitAvatars.clear();
 
   currentState = 0;
+}
+
+//Función para renderizar los productos
+const renderProducts = (category:number)=>{
+  if (mainProductsItems) {
+    mainProductsItems.innerHTML = ''
+
+    const data = category==1? productsDatabase.tortas : category==2? productsDatabase.malteadas : productsDatabase.helados;
+  
+    data.forEach(item=>{
+      const newItem = `
+      <div class="products_item">
+      <img src="${item.imgUrl}" alt="${item.name}" class="w-full h-full object-cover object-center rounded-[20px]">
+
+      <span class="absolute left-1/2 -translate-x-1/2 -bottom-12 w-40">
+        <h2 class="main_title text-base bg-white text-center px-4 py-1 w-full rounded-2xl relative">${item.name}</h2>
+        <p class="main_title text-[12px] relative bg-gray rounded-b-xl w-fit px-4 py-1 mx-auto">$ ${item.prize}</p>
+      </span>
+      </div>
+      `
+
+    mainProductsItems.innerHTML += newItem;
+    })
+
+    gsap.from('.products_item', {
+    opacity: 0,
+    x: -100,
+    duration: 0.5,
+    ease: 'sine.out',
+    stagger: {
+      each: 0.5,
+      from: 'end',
+      }
+    })
+  }
 }
 
 const animations = async (newState:number)=>{
@@ -159,16 +273,20 @@ const animations = async (newState:number)=>{
     animationsRotate(newState);
     await playTimeline(tlRotate)
 
-    if (previousWidth >= 1024) {
+    if (isDesktop) {
       animationsProducts();
       await playTimeline(tlProducts)
+      renderProducts(newState);
     }
   }
   if (currentState != 0) {
     await tlRotate.reverse();
     tlRotate.clear();
+
+    renderProducts(newState);
+
     animationsRotate(newState);
-    await playTimeline(tlRotate)
+    await playTimeline(tlRotate);
   }
 
   currentState = newState;
@@ -188,11 +306,14 @@ mainHomeBtn?.addEventListener('click', ()=>{
 })
 window.addEventListener('resize', () => {
   const currentWidth = window.innerWidth;
+  const lastIsDesktop = isDesktop;
   
-  if (currentWidth !== previousWidth) {
-    previousWidth = currentWidth;
+  if (currentWidth >= 1024) {
+    isDesktop = true;
+  } else {
+    isDesktop = false;
   }
-  if (currentWidth < 1024) {
+  if (lastIsDesktop != isDesktop) {
     reverseAnimations();
   }
 });
