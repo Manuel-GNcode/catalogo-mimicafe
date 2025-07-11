ac57c1aff48b7e151442c1c3f06d4a07a23f258b
GNcode <gngalarga027@gmail.com>
2025-07-10 19:45:14 -0500

    feat: adjust layout positioning for products and home sections, enhance animations for smoother transitions

--- src/main.ts
+++ src/main.ts
@@ -107,20 +107,24 @@ const tlProducts = gsap.timeline({
 const animationsProducts = ()=>{
   tlProducts.to('#main_ctr', {
       left: '10%',
-    }).set('#main_products', {
+    })
+    .set('#main_products', {
       opacity: 1,
     }).to('#main_products', {
       width: '75%',
-    }, '<').to('#main_home', {
-      top: '-12%',
-      right: '+=7%'
+    }, '<')
+    .to('#main_home', {
+      bottom: '110%',
+      yPercent: 100,
+      xPercent: -20,
     }, '<').to('.main_home-span', {
-      rotate: '-=70'
-    }, '<').to('#main_bitacora', {
-      left: '+=85%',
-      bottom: '+=5%'
+      rotate: '-=90'
+    }, '<')
+    .to('#main_bitacora', {
+      left: '105%',
+      xPercent: -120,
     }, '<').to('.main_bitacora-span', {
-      rotate: '-=120'
+      rotate: '-=90'
     }, '<').set('#main_home-btn', {
       display: 'block'
     })
