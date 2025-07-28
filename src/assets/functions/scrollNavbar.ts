13775d1607658546bd440aa61bfc31b7573f5008
GNcode <gngalarga027@gmail.com>
2025-07-28 14:00:20 -0500

    feat: enhance navbar functionality with GSAP animations and improve accessibility with aria-labels

--- src/assets/functions/scrollNavbar.ts
+++ src/assets/functions/scrollNavbar.ts
@@ -1,27 +1,44 @@
 import gsap from "gsap";
 import ScrollTrigger from "gsap/dist/ScrollTrigger";
 
+const navbarBtn = document.getElementById('navbar_btn');
+
 gsap.registerPlugin(ScrollTrigger);
 
+let isMenuOn = false;
+
 export const scrollNavbar = ()=>{
+  const btnAnimation = gsap.from("#navbar a", {
+    paused: true,
+    x: 150,
+    stagger: 0.1,
+    duration: 0.4,
+    ease: 'sine.out',
+  })
   gsap.to("#navbar", {
+    xPercent: -100,
     scrollTrigger: {
       trigger: "#navbar",
       start: "top top",
       endTrigger: "#mobile_helados",
       end: "bottom bottom-=300",
       pin: true,
+      toggleActions: 'play reverse play reverse'
+    },
+    onReverseComplete: ()=>{
+      btnAnimation.reverse();
+      isMenuOn = false;
     }
   });
-  gsap.from("#navbar a", {
-    x: 150,
-    stagger: 0.1,
-    duration: 0.4,
-    ease: 'sine.out',
-    scrollTrigger: {
-      trigger: '#mobile_tortas',
-      start: 'top-=50 top',
-      toggleActions: "play none none reverse",
+
+  navbarBtn?.addEventListener('click', ()=>{
+    if (isMenuOn) {
+      btnAnimation.reverse();
+      isMenuOn = false;
+    }
+    else {
+      btnAnimation.restart();
+      isMenuOn = true;
     }
   })
 }
